980971eb8ba8025959468e3ea247d1789b912f61
Blank21-xD <lama.safal21@gmail.com>
2025-12-23 18:20:03 +1100

    Force adding build.sh despite gitignore

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+pip install -r requirements.txt
+
+python manage.py collectstatic --noinput
+python manage.py migrate
+
+# Create superuser if the variables are set
+if [ "$DJANGO_SUPERUSER_USERNAME" ]; then
+  python manage.py createsuperuser \
+    --no-input \
+    --username "$DJANGO_SUPERUSER_USERNAME" \
+    --email "$DJANGO_SUPERUSER_EMAIL" || true
+fi
