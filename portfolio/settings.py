93452fdab34c7e2eaa8d64532995054890544f3d
Blank21-xD <lama.safal21@gmail.com>
2025-12-23 18:16:08 +1100

    Switching to build script

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -52,14 +52,23 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 # DATABASE CONFIG
 # Uses PostgreSQL on Render and SQLite locally
-DATABASES = {
-    'default': dj_database_url.config(
-        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
-        conn_max_age=600,
-        engine='django.db.backends.postgresql'
-    )
-}
-
+# Check if we are on Render (which provides a DATABASE_URL)
+if os.getenv('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.config(
+            conn_max_age=600,
+            conn_health_checks=True,
+            engine='django.db.backends.postgresql'
+        )
+    }
+else:
+    # Use simple SQLite locally
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
