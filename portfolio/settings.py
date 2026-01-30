2b80d5978db7935683129406bc385e1ad3225a46
Blank21-xD <lama.safal21@gmail.com>
2026-01-30 23:12:56 +1100

    Fixing static paths for deployment

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -90,9 +90,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "main" / "static",
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / "staticfiles"
 if not DEBUG:
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
